6819e0b3e9e5f42b2cc4a2789887a272a2eaa5c7
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-08 23:47:59 +0530

    Update camera.js

--- app/modules/camera.js
+++ app/modules/camera.js
@@ -1,1 +1,19 @@
 
+export async function startCamera(video){
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+
+        video:{
+            facingMode:"environment",
+            width:{ideal:1280},
+            height:{ideal:720}
+        },
+
+        audio:false
+
+    })
+
+    video.srcObject = stream
+
+    await video.play()
+}
