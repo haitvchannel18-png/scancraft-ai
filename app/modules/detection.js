2c598c2001e14dfad8e9335aa57a6ffaff112562
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-08 23:47:28 +0530

    Update detection.js

--- app/modules/detection.js
+++ app/modules/detection.js
@@ -1,1 +1,26 @@
+let model
+let detecting = false
 
+export async function loadModel(){
+
+    console.log("Loading AI detection model...")
+
+    model = await cocoSsd.load({
+        base: "lite_mobilenet_v2"
+    })
+
+    console.log("AI Model Ready")
+}
+
+export async function detectFrame(video){
+
+    if(!model || detecting) return []
+
+    detecting = true
+
+    const predictions = await model.detect(video)
+
+    detecting = false
+
+    return predictions
+}
