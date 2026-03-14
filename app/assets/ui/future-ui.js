e5d100b35fa383f7e06acff477390b26a199e715
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-14 10:25:02 +0530

    Update future-ui.js

--- app/assets/ui/future-ui.js
+++ app/assets/ui/future-ui.js
@@ -1,1 +1,67 @@
+// ScanCraft AI
+// Future Prediction Visualization UI
 
+import AnimationEngine from "../animations/ui-animations.js"
+import EventBus from "../../modules/core/events.js"
+
+class FutureUI {
+
+constructor(){
+
+this.panel = document.getElementById("futurePanel")
+
+this.init()
+
+}
+
+init(){
+
+EventBus.on("FUTURE_RESULT", data => {
+
+this.showFuture(data)
+
+})
+
+}
+
+showFuture(data){
+
+this.panel.innerHTML=""
+
+const title = document.createElement("h2")
+
+title.innerText="Future Possibilities"
+
+this.panel.appendChild(title)
+
+data.images.forEach(img => {
+
+const card = document.createElement("div")
+
+card.className="future-card"
+
+card.innerHTML = `
+
+<img src="${img.url}" />
+
+<p>${img.description}</p>
+
+`
+
+this.panel.appendChild(card)
+
+AnimationEngine.openPanel(card)
+
+})
+
+}
+
+hide(){
+
+AnimationEngine.closePanel(this.panel)
+
+}
+
+}
+
+export default new FutureUI()
