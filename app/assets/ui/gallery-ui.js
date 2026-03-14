c47feef34f6eda54ee9e5d2b705d10aa2aabae28
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-14 10:31:35 +0530

    Update gallery-ui.js

--- app/assets/ui/gallery-ui.js
+++ app/assets/ui/gallery-ui.js
@@ -1,1 +1,89 @@
+// ScanCraft AI
+// Advanced Object Gallery UI
 
+import AnimationEngine from "../animations/ui-animations.js"
+import EventBus from "../../modules/core/events.js"
+
+class GalleryUI {
+
+constructor(){
+
+this.panel = document.getElementById("galleryPanel")
+this.grid = document.getElementById("galleryGrid")
+
+this.items = []
+
+this.init()
+
+}
+
+init(){
+
+EventBus.on("ADD_TO_GALLERY",(data)=>{
+
+this.addItem(data)
+
+})
+
+EventBus.on("CLEAR_GALLERY",()=>{
+
+this.clear()
+
+})
+
+}
+
+addItem(data){
+
+this.items.unshift(data)
+
+const card = document.createElement("div")
+card.className = "gallery-card"
+
+card.innerHTML = `
+
+<div class="gallery-thumb">
+<img src="${data.image}" />
+</div>
+
+<div class="gallery-meta">
+<h4>${data.name || "Unknown Object"}</h4>
+<p>${data.time || ""}</p>
+</div>
+
+`
+
+card.addEventListener("click",()=>{
+
+EventBus.emit("OPEN_OBJECT_PANEL",data)
+
+})
+
+this.grid.prepend(card)
+
+AnimationEngine.openPanel(card)
+
+}
+
+clear(){
+
+this.grid.innerHTML = ""
+this.items = []
+
+}
+
+show(){
+
+AnimationEngine.openPanel(this.panel)
+
+}
+
+hide(){
+
+AnimationEngine.closePanel(this.panel)
+
+}
+
+}
+
+export default new GalleryUI()
