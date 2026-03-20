1f3860984dc50069fbad5d1402b28805cb9ef27b
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:18:05 +0530

    Update variation-generator.js

--- app/modules/generation/variation-generator.js
+++ app/modules/generation/variation-generator.js
@@ -1,1 +1,35 @@
+// modules/generation/variation-generator.js
 
+import AILogger from "../utils/ai-logger.js"
+import ImageGenerator from "./image-generator.js"
+
+class VariationGenerator {
+
+async generateVariations(object){
+
+const prompts = [
+`${object} futuristic design`,
+`${object} premium version`,
+`${object} minimal style`,
+`${object} concept design`
+]
+
+const results = []
+
+for(const p of prompts){
+
+const img = await ImageGenerator.generate(p)
+
+if(img) results.push(img)
+
+}
+
+AILogger.log("info","Variations generated",{object})
+
+return results
+
+}
+
+}
+
+export default new VariationGenerator()
