89e8b6b97985933c75610c204c98d01e2dac31be
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 15:22:57 +0530

    Update user.js

--- app/modules/auth/user.js
+++ app/modules/auth/user.js
@@ -1,1 +1,25 @@
+// modules/auth/user.js
 
+import Session from "./session.js"
+
+class User {
+
+getId(){
+return Session.get()?.uid || null
+}
+
+getName(){
+return Session.get()?.displayName || "Guest"
+}
+
+getEmail(){
+return Session.get()?.email || null
+}
+
+isLoggedIn(){
+return !!Session.get()
+}
+
+}
+
+export default new User()
