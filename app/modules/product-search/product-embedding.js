10195224a9dd00b432060dcdd7b9f08caa423a8f
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:25:32 +0530

    Update product-embedding.js

--- app/modules/product-search/product-embedding.js
+++ app/modules/product-search/product-embedding.js
@@ -1,1 +1,23 @@
+// modules/product-search/product-embedding.js
 
+import AILogger from "../utils/ai-logger.js"
+
+class ProductEmbedding {
+
+generate(object){
+
+// 🧠 simple embedding simulation (future CLIP/real embedding)
+const vector = object
+.toLowerCase()
+.split("")
+.map(c => c.charCodeAt(0) / 255)
+
+AILogger.log("info","Product embedding created",{object})
+
+return vector
+
+}
+
+}
+
+export default new ProductEmbedding()
