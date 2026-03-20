9c6aefd9a5b2346832a91abe693c71e39e6af502
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:38:46 +0530

    Update result-ranker.js

--- app/modules/reasoning/result-ranker.js
+++ app/modules/reasoning/result-ranker.js
@@ -1,1 +1,24 @@
+// modules/reasoning/result-ranker.js
 
+class ResultRanker {
+
+finalize(objects){
+
+return objects.map(o=>({
+
+...o,
+
+finalScore:
+(o.confidence * 0.5) +
+(o.importance * 0.3) +
+((o.count || 1) * 0.1) +
+(Math.random()*0.1)
+
+}))
+.sort((a,b)=>b.finalScore - a.finalScore)
+
+}
+
+}
+
+export default new ResultRanker()
