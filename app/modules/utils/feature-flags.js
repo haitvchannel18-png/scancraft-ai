376b800d647e4a1bfae5622159ca1d3aa9c26f18
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 12:57:27 +0530

    Update feature-flags.js

--- app/modules/utils/feature-flags.js
+++ app/modules/utils/feature-flags.js
@@ -1,1 +1,55 @@
+// modules/utils/feature-flags.js
 
+import ConfigManager from "./config-manager.js"
+
+class FeatureFlags {
+
+constructor(){
+this.flags = structuredClone(ConfigManager.get("FEATURES") || {})
+}
+
+// 🔍 check
+isEnabled(feature){
+
+return !!this.flags[feature]
+
+}
+
+// 🔄 enable
+enable(feature){
+
+this.flags[feature] = true
+
+}
+
+// ❌ disable
+disable(feature){
+
+this.flags[feature] = false
+
+}
+
+// 🔁 toggle
+toggle(feature){
+
+this.flags[feature] = !this.flags[feature]
+
+}
+
+// ⚙️ sync with config
+sync(){
+
+this.flags = structuredClone(ConfigManager.get("FEATURES") || {})
+
+}
+
+// 📊 get all
+getAll(){
+
+return this.flags
+
+}
+
+}
+
+export default new FeatureFlags()
