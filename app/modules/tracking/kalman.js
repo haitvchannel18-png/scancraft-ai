e16017a5975c3726d6be98d39a05a3c975fa0f94
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:33:11 +0530

    Update kalman.js

--- app/modules/tracking/kalman.js
+++ app/modules/tracking/kalman.js
@@ -1,1 +1,28 @@
+// modules/tracking/kalman.js
 
+class KalmanFilter {
+
+constructor(){
+this.x = 0
+this.p = 1
+this.q = 0.01
+this.r = 0.1
+}
+
+update(measurement){
+
+// prediction
+this.p += this.q
+
+// update
+const k = this.p / (this.p + this.r)
+this.x += k * (measurement - this.x)
+this.p *= (1 - k)
+
+return this.x
+
+}
+
+}
+
+export default KalmanFilter
