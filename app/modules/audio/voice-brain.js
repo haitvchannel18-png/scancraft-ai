bd97e242f8c2371d4302f8e566d322edf9de82c4
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 14:14:21 +0530

    Update voice-brain.js

--- app/modules/audio/voice-brain.js
+++ app/modules/audio/voice-brain.js
@@ -1,1 +1,33 @@
+// modules/audio/voice-brain.js
 
+import VoiceEngine from "./voice-engine-pro.js"
+
+class VoiceBrain {
+
+speakInsight(data){
+
+let text = ""
+
+if(data.object){
+text += `This looks like a ${data.object}. `
+}
+
+if(data.material){
+text += `It is made of ${data.material}. `
+}
+
+if(data.category){
+text += `Category is ${data.category}. `
+}
+
+if(data.confidence){
+text += `Confidence is ${Math.round(data.confidence*100)} percent.`
+}
+
+VoiceEngine.speak(text)
+
+}
+
+}
+
+export default new VoiceBrain()
