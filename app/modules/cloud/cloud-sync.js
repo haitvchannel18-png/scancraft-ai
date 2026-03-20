cd4a3a671d0cd4e9af4b9f9935a100c22ab5dffb
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 15:22:33 +0530

    Update cloud-sync.js

--- app/modules/cloud/cloud-sync.js
+++ app/modules/cloud/cloud-sync.js
@@ -1,1 +1,38 @@
+// modules/cloud/cloud-sync.js
 
+import { db } from "../auth/firebase.js"
+import {
+doc,
+setDoc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js"
+
+class CloudSync {
+
+// 💾 SAVE DATA
+async save(userId, key, data){
+
+await setDoc(doc(db, key, userId), {
+data,
+updatedAt: Date.now()
+})
+
+}
+
+// 📥 GET DATA
+async get(userId, key){
+
+const ref = doc(db, key, userId)
+const snap = await getDoc(ref)
+
+if(snap.exists()){
+return snap.data().data
+}
+
+return null
+
+}
+
+}
+
+export default new CloudSync()
