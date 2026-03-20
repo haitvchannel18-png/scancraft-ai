59a79ba74db3e55d312a209a80591b8d55dbc5da
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:11:14 +0530

    Update user-feedback.js

--- app/modules/learning/user-feedback.js
+++ app/modules/learning/user-feedback.js
@@ -1,1 +1,21 @@
+// modules/learning/user-feedback.js
 
+import LearningEngine from "./learning-engine.js"
+
+class UserFeedback {
+
+submit({object, result, feedback}){
+
+LearningEngine.process({
+object,
+result,
+feedback
+})
+
+console.log("👍 Feedback submitted:", feedback)
+
+}
+
+}
+
+export default new UserFeedback()
