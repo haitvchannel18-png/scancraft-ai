1f23da1517884d805452a1531f4304383c912331
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:06:43 +0530

    Update knowledge-expander.js

--- app/modules/learning-ai/knowledge-expander.js
+++ app/modules/learning-ai/knowledge-expander.js
@@ -1,1 +1,23 @@
+// modules/learning-ai/knowledge-expander.js
 
+import KnowledgeUpdater from "./knowledge-updater.js"
+import AILogger from "../utils/ai-logger.js"
+
+class KnowledgeExpander {
+
+expand(object, inferredData){
+
+if(!object) return
+
+KnowledgeUpdater.update(object, {
+...inferredData,
+source: "ai_generated"
+})
+
+AILogger.log("info","Knowledge expanded",{object})
+
+}
+
+}
+
+export default new KnowledgeExpander()
