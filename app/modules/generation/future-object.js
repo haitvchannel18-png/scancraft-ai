2607f505bc262b0fa22b3fff5aa9dc8c2552917b
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:18:29 +0530

    Update future-object.js

--- app/modules/generation/future-object.js
+++ app/modules/generation/future-object.js
@@ -1,1 +1,25 @@
+// modules/generation/future-object.js
 
+import AILogger from "../utils/ai-logger.js"
+import ImageGenerator from "./image-generator.js"
+
+class FutureObject {
+
+async predict(object){
+
+const prompt = `${object} in future advanced technology 2050`
+
+const image = await ImageGenerator.generate(prompt)
+
+AILogger.log("info","Future object generated",{object})
+
+return {
+object,
+futureImage: image
+}
+
+}
+
+}
+
+export default new FutureObject()
