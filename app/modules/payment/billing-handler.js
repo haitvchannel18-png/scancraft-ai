0901feb5aecb7b1cf6aa28370f605e93326a8af1
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 15:50:04 +0530

    Update billing-handler.js

--- app/modules/payment/billing-handler.js
+++ app/modules/payment/billing-handler.js
@@ -1,1 +1,28 @@
+// modules/payment/billing-handler.js
 
+import PaymentManager from "./payment-manager.js"
+
+class BillingHandler {
+
+init(){
+
+// Android se callback
+window.onPurchaseSuccess = (data)=>{
+
+console.log("✅ Purchase Success:", data)
+
+PaymentManager.unlockPro()
+
+}
+
+window.onPurchaseFailed = (err)=>{
+
+console.error("❌ Purchase Failed:", err)
+
+}
+
+}
+
+}
+
+export default new BillingHandler()
