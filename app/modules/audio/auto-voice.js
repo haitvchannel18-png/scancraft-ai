ecb2ceec3c0488d6e62d7e3da12e08d1efc72a88
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 14:15:03 +0530

    Update auto-voice.js

--- app/modules/audio/auto-voice.js
+++ app/modules/audio/auto-voice.js
@@ -1,1 +1,18 @@
+// modules/audio/auto-voice.js
 
+import { EventBus } from "../core/events.js"
+import VoiceBrain from "./voice-brain.js"
+
+class AutoVoice {
+
+init(){
+
+EventBus.on("reasoningComplete",(data)=>{
+VoiceBrain.speakInsight(data)
+})
+
+}
+
+}
+
+export default new AutoVoice()
