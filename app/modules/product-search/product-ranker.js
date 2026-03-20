0c05c6a45d126a568aa636db6648e089126fad9f
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:26:16 +0530

    Update product-ranker.js

--- app/modules/product-search/product-ranker.js
+++ app/modules/product-search/product-ranker.js
@@ -1,1 +1,33 @@
+// modules/product-search/product-ranker.js
 
+import { cosineSimilarity } from "../utils/helpers.js"
+import ProductEmbedding from "./product-embedding.js"
+import AILogger from "../utils/ai-logger.js"
+
+class ProductRanker {
+
+rank(object, products){
+
+const queryVec = ProductEmbedding.generate(object)
+
+const ranked = products.map(p=>{
+
+const vec = ProductEmbedding.generate(p.name)
+
+return {
+...p,
+score: cosineSimilarity(queryVec, vec)
+}
+
+})
+.sort((a,b)=>b.score - a.score)
+
+AILogger.log("info","Products ranked",{object})
+
+return ranked
+
+}
+
+}
+
+export default new ProductRanker()
