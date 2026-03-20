4dcf694ee508d64fb236f64a95fbb6cce156213e
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:21:44 +0530

    Update object-select.js

--- app/modules/interaction/object-select.js
+++ app/modules/interaction/object-select.js
@@ -1,1 +1,39 @@
+// modules/interaction/object-select.js
 
+import { EventBus } from "../core/events.js"
+import AILogger from "../utils/ai-logger.js"
+
+class ObjectSelect {
+
+constructor(){
+this.selected = null
+}
+
+// 🎯 select object
+select(object){
+
+this.selected = object
+
+EventBus.emit("objectSelected", object)
+
+AILogger.log("info","Object selected",{object})
+
+}
+
+// ❌ clear selection
+clear(){
+
+this.selected = null
+
+EventBus.emit("objectCleared")
+
+}
+
+// 📦 get selected
+get(){
+return this.selected
+}
+
+}
+
+export default new ObjectSelect()
