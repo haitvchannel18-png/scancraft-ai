53be34405e1ee43737edb2973ec57aa8054a4afe
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:05:39 +0530

    Update memory-sync.js

--- app/modules/learning-ai/memory-sync.js
+++ app/modules/learning-ai/memory-sync.js
@@ -1,1 +1,31 @@
+// modules/learning-ai/memory-sync.js
 
+import ConversationLearning from "./conversation-learning.js"
+import PatternLearner from "./pattern-learner.js"
+import KnowledgeUpdater from "./knowledge-updater.js"
+
+class MemorySync {
+
+sync(){
+
+ConversationLearning.save()
+PatternLearner.save()
+KnowledgeUpdater.save()
+
+console.log("🧠 Memory Synced")
+
+}
+
+load(){
+
+ConversationLearning.load()
+PatternLearner.load()
+KnowledgeUpdater.load()
+
+console.log("🧠 Memory Loaded")
+
+}
+
+}
+
+export default new MemorySync()
