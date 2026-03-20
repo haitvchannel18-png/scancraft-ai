7efb4f294937a2312004db437470f030164e9108
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 15:10:14 +0530

    Update auth.js

--- app/modules/auth/auth.js
+++ app/modules/auth/auth.js
@@ -1,1 +1,32 @@
+// modules/auth/auth.js
 
+import { auth } from "./firebase.js"
+import {
+GoogleAuthProvider,
+signInWithPopup,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
+
+class Auth {
+
+async login(){
+
+const provider = new GoogleAuthProvider()
+const result = await signInWithPopup(auth, provider)
+
+return result.user
+
+}
+
+logout(){
+return signOut(auth)
+}
+
+onChange(callback){
+onAuthStateChanged(auth, callback)
+}
+
+}
+
+export default new Auth()
