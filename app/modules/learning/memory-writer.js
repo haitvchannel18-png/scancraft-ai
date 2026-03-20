c52b37987ddf0f1d1d4baa4df38037f47b5e2159
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:40:54 +0530

    Update memory-writer.js

--- app/modules/learning/memory-writer.js
+++ app/modules/learning/memory-writer.js
@@ -1,1 +1,30 @@
+// modules/learning/memory-writer.js
 
+import AILogger from "../utils/ai-logger.js"
+
+class MemoryWriter {
+
+constructor(){
+this.memory = []
+}
+
+// 🔥 store new knowledge
+save(entry){
+
+this.memory.push({
+...entry,
+timestamp: Date.now()
+})
+
+AILogger.log("info","Memory saved",entry)
+
+}
+
+// 🔍 get memory
+getAll(){
+return this.memory
+}
+
+}
+
+export default new MemoryWriter()
