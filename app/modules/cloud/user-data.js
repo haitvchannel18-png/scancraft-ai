efba2f5044a5118776a19e22209c8ea8ba2f2dda
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 15:21:52 +0530

    Update user-data.js

--- app/modules/cloud/user-data.js
+++ app/modules/cloud/user-data.js
@@ -1,1 +1,28 @@
+// modules/cloud/user-data.js
 
+import CloudSync from "./cloud-sync.js"
+
+class UserData {
+
+// 💾 SAVE USER PROFILE
+async saveProfile(user){
+
+await CloudSync.save(user.uid, "profile", {
+name: user.displayName,
+email: user.email,
+photo: user.photoURL,
+lastLogin: Date.now()
+})
+
+}
+
+// 📥 GET PROFILE
+async getProfile(userId){
+
+return await CloudSync.get(userId, "profile")
+
+}
+
+}
+
+export default new UserData()
