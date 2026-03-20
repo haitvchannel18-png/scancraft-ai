5cf7bc27426e3c9fa4f5e7791f75338f2ec45a69
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:33:53 +0530

    Update tracking.js

--- app/modules/tracking/tracking.js
+++ app/modules/tracking/tracking.js
@@ -1,1 +1,30 @@
+// modules/tracking/tracking.js
 
+import KalmanFilter from "./kalman.js"
+import MotionTracker from "./motion.js"
+
+class TrackingSystem {
+
+constructor(){
+this.kx = new KalmanFilter()
+this.ky = new KalmanFilter()
+}
+
+track(point){
+
+const smoothX = this.kx.update(point.x)
+const smoothY = this.ky.update(point.y)
+
+const speed = MotionTracker.detect({x:smoothX, y:smoothY})
+
+return {
+x: smoothX,
+y: smoothY,
+speed
+}
+
+}
+
+}
+
+export default new TrackingSystem()
