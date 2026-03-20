89ea780ed9c104f6ef4900fb590b8645603a86db
haitvchannel18-png <haitvchannel18@gmail.com>
2026-03-20 13:22:30 +0530

    Update brain-menu.js

--- app/modules/interaction/brain-menu.js
+++ app/modules/interaction/brain-menu.js
@@ -1,1 +1,39 @@
+// modules/interaction/brain-menu.js
 
+import FeatureFlags from "../utils/feature-flags.js"
+import AILogger from "../utils/ai-logger.js"
+
+class BrainMenu {
+
+toggle(feature){
+
+FeatureFlags.toggle(feature)
+
+AILogger.log("info","Feature toggled",{feature})
+
+}
+
+// 🎛 UI bind
+init(){
+
+const el = document.getElementById("brain-menu")
+
+if(!el) return
+
+Object.keys(FeatureFlags.getAll()).forEach(f=>{
+
+const btn = document.createElement("button")
+
+btn.innerText = f
+
+btn.onclick = ()=>this.toggle(f)
+
+el.appendChild(btn)
+
+})
+
+}
+
+}
+
+export default new BrainMenu()
